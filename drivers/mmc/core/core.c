12fbadf3e2264ab933cb1724aff606e1651ded14
Dong Chuanxiao <chuanxiao.dong@intel.com>
2013-07-17 15:13:05 +0800

    mmc: init eMMC card in sync mode
    Since adb device ID is depends on eMMC CID, let MMC driver init
    eMMC card in sync mode can make sure eMMC CID be got by USB driver
    correctly.
    In the meanwhile, it is also can make sure before IPC send command
    to SCU to access eMMC card, eMMC card is initliazed already.
    
    Fixed conflict porting 3.10 to 4.x
    
    Signed-off-by: Ning Feiyi <feiyix.ning@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    
    Conflicts:
    	drivers/mmc/core/core.c
    	include/linux/mmc/host.h
    
    Change-Id: Iaefe49039680ad2506db6ab7d40c6a48f5710852

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2525,6 +2525,8 @@ void mmc_start_host(struct mmc_host *host)
 		mmc_power_up(host, host->ocr_avail);
 	mmc_gpiod_request_cd_irq(host);
 	_mmc_detect_change(host, 0, false);
+	if (host->caps2 & MMC_CAP2_INIT_CARD_SYNC)
+		flush_work_sync(&host->detect.work);
 }
 
 void mmc_stop_host(struct mmc_host *host)
