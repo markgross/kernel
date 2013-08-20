f9df3dd87a7f1e3fc5a89f600fb59a0c0f5c700a
Wang, Yu <yu.y.wang@intel.com>
2013-08-20 18:28:44 +0800

    dwc3-host: Support DWC3 host mode on Intel Merrifield platform
    Due to xhci-plat.c have not a good support for xHCI driver register as
    platform device driver. Special for power management. So write one
    separate driver for DWC3 host mode as temporary.
    
    This issue will be discuss with community. After xhci-plat driver
    support powr management. Will follow it to remove this temporary
    solution.
    
    Fixed conflict porting 3.10 to 4.x
    
    Signed-off-by: Wang, Yu <yu.y.wang@intel.com>
    
    Conflicts:
    	drivers/usb/dwc3/Kconfig
    	drivers/usb/host/xhci-plat.c
    
    Change-Id: If8180f928cf7fef1d7f72b67adaf2c121dd86f5f

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -275,10 +275,26 @@ static int __init xhci_plat_init(void)
 	xhci_plat_hc_driver.start = xhci_plat_start;
 	return platform_driver_register(&usb_xhci_driver);
 }
+
+#ifdef CONFIG_USB_DWC3_HOST_INTEL
+#include "../dwc3/dwc3-host-intel.c"
+#endif
+
+int xhci_register_plat(void)
+{
+#ifdef CONFIG_USB_DWC3_HOST_INTEL
+	return platform_driver_register(&dwc3_xhci_driver);
+#endif
+	return platform_driver_register(&usb_xhci_driver);
+}
 module_init(xhci_plat_init);
 
 static void __exit xhci_plat_exit(void)
 {
+#ifdef CONFIG_USB_DWC3_HOST_INTEL
+	platform_driver_unregister(&dwc3_xhci_driver);
+	return;
+#endif
 	platform_driver_unregister(&usb_xhci_driver);
 }
 module_exit(xhci_plat_exit);
