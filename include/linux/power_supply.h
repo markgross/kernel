9560932b00d4d1cae690b225ff5d8a3ac2021c24
Todd Poynor <toddpoynor@google.com>
2013-12-12 15:59:09 -0800

    power: Add property CHARGE_COUNTER_EXT and 64-bit precision properties
    Add POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT that stores accumulated charge
    in nAh units as a signed 64-bit value.
    
    Add generic support for signed 64-bit property values.
    
    Change-Id: I2bd34b1e95ffba24e7bfef81f398f22bd2aaf05e
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -18,6 +18,7 @@
 #include <linux/leds.h>
 #include <linux/spinlock.h>
 #include <linux/notifier.h>
+#include <linux/types.h>
 
 /*
  * All voltages, currents, charges, energies, time and temperatures in uV,
@@ -160,6 +161,8 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_USB_HC,
 	POWER_SUPPLY_PROP_USB_OTG,
 	POWER_SUPPLY_PROP_CHARGE_ENABLED,
+	/* Local extensions of type int64_t */
+	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
 	POWER_SUPPLY_PROP_MODEL_NAME,
 	POWER_SUPPLY_PROP_MANUFACTURER,
@@ -228,6 +231,7 @@ struct power_supply_cable_props {
 union power_supply_propval {
 	int intval;
 	const char *strval;
+	int64_t int64val;
 };
 
 struct device_node;
