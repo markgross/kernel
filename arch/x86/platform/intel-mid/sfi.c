beb5dd0d1d174d9b3b3c040f8168175983d4f466
Saadi Maalem <saadi.maalem@intel.com>
2014-02-20 20:50:43 +0100

    EDISON: Remove SPID
    The SPID mechanism is not needed for EDISON.
    Actually, better not to have it at all to simplify the code
    and avoid the need of provisionning SPID on manufacturing line.
    
    Remove the SPID implementation and all references to SPID macros in the code.
    
    Fixed conflict porting 3.10 to 4.x (commit will be dropped for Edison along with previous commits that added the SPID handling code in the first place).
    
    Conflicts:
    	arch/x86/platform/intel-mid/device_libs/platform_hsu.c
    	arch/x86/platform/intel-mid/device_libs/platform_mid_pwm.c
    	arch/x86/platform/intel-mid/device_libs/platform_mrfl_thermal.c
    	arch/x86/platform/intel-mid/device_libs/platform_msic_thermal.c
    	arch/x86/platform/intel-mid/device_libs/platform_soc_thermal.c
    	arch/x86/platform/intel-mid/intel_mid_sfi.c
    	arch/x86/platform/intel-mid/spid.c
    
    Change-Id: I2febcd01cbf0570511cbc395d86e8dcc62d3426f

--- arch/x86/platform/intel-mid/sfi.c
+++ arch/x86/platform/intel-mid/sfi.c
@@ -42,7 +42,6 @@
 #include <asm/intel_scu_ipc.h>
 #include <asm/apb_timer.h>
 #include <asm/reboot.h>
-#include <asm/spid.h>
 
 #define	SFI_SIG_OEM0	"OEM0"
 #define MAX_IPCDEVS	24
@@ -535,9 +534,6 @@ static int __init sfi_parse_oemb(struct sfi_table_header *table)
 	u8 sig[SFI_SIGNATURE_SIZE + 1] = {'\0'};
 	u8 oem_id[SFI_OEM_ID_SIZE + 1] = {'\0'};
 	u8 oem_table_id[SFI_OEM_TABLE_ID_SIZE + 1] = {'\0'};
-
-	/* parse SPID and SSN out from OEMB table */
-	sfi_handle_spid(table);
 
 	oemb = (struct sfi_table_oemb *) table;
 	if (!oemb) {
