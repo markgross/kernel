39c435ce18b376a28afeb69cb99d4dc92f139a34
Chuanxiao Dong <chuanxiao.dong@intel.com>
2016-05-06 10:41:49 -0700

    mmc: core_sdhci: emmc mutex support for MFLD/MRFLD
    One of the Medfield/Merrifield eMMC controller (PCI device id 0x0823, SDIO3)
    is a shared resource used by the SCU and the IA processors.
    SCU primarily uses the eMMC host controller to access the eMMC device's
    Boot Partition, while the IA CPU uses the eMMC host controller to
    access the eMMC device's User Partition.
    
    After the SCU hands off the system to the IA processor, the IA processor
    assumes ownership to the eMMC host controller. Due to absence of any
    arbitration at the eMMC host controller, this could result in concurrent
    eMMC host accesses resulting in bus contention and garbage data ending up
    in either of the partitions.
    
    To circumvent this from happening, eMMC host controller locking mechanism
    is employed, where at any one given time, only one agent, SCU or IA,
    may be allowed to access the host. This is achieved by implementing
    Dekker's Algorithm (http://en.wikipedia.org/wiki/Dekker's_algorithm)
    between the two processors.
    
    Before handing off the system to the IA processor, SCU must set up three
    housekeeping mutex variables allocated in the shared SRAM as follows:
    
    eMMC_Owner = IA (SCU and IA processors - RW, 32bit)
    IA_Req = FALSE (IA -RW, SCU - RO, 32bit)
    SCU_Req = FALSE (IA - RO, SCU - R/W, 32bit)
    
    There is no hardware based access control to these variables and so code
    executing on SCU and IA processors must follow access rules of
    Dekker's algorithm. Detail info about this algorithm can be found in
    comments and code of this patch.
    
    Change-Id: I97a003cfc5d0b78c23e3a5ec5034cb35e6916d9a
    Signed-off-by: Brian Wood <brian.j.wood@intel.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2761,6 +2761,56 @@ EXPORT_SYMBOL(mmc_flush_cache);
 
 #ifdef CONFIG_PM
 
+/**
+ *      mmc_suspend_host - suspend a host
+ *      @host: mmc host
+ */
+int mmc_suspend_host(struct mmc_host *host)
+{
+        int err = 0;
+
+        cancel_delayed_work(&host->detect);
+        mmc_flush_scheduled_work();
+
+        mmc_bus_get(host);
+        if (host->bus_ops && !host->bus_dead) {
+                if (host->bus_ops->suspend) {
+                        if (mmc_card_doing_bkops(host->card)) {
+                                err = mmc_stop_bkops(host->card);
+                                if (err)
+                                        goto out;
+                        }
+                        err = host->bus_ops->suspend(host);
+                }
+
+                if (err == -ENOSYS || !host->bus_ops->resume) {
+                        /*
+                         * We simply "remove" the card in this case.
+                         * It will be redetected on resume.  (Calling
+                         * bus_ops->remove() with a claimed host can
+                         * deadlock.)
+                         */
+                        if (host->bus_ops->remove)
+                                host->bus_ops->remove(host);
+                        mmc_claim_host(host);
+                        mmc_detach_bus(host);
+                        mmc_power_off(host);
+                        mmc_release_host(host);
+                        host->pm_flags = 0;
+                        err = 0;
+                }
+        }
+        mmc_bus_put(host);
+
+        if (!err && !mmc_card_keep_power(host))
+                mmc_power_off(host);
+
+out:
+        return err;
+}
+
+EXPORT_SYMBOL(mmc_suspend_host);
+
 /* Do the card removal on suspend if card is assumed removeable
  * Do that in pm notifier while userspace isn't yet frozen, so we will be able
    to sync the card.
